5b96deef9361a0ad4b4ea9e1799cbaf6d765e032
Joseph-Odhiambo <josephkdo@gmail.com>
2020-10-16 12:41:31 +0300

    env secret key

--- instaclone/settings.py
+++ instaclone/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '0ty2o7sny@z_8s*_90abx5j!+6(^rt50*68lut-@(e*evm^pp!'
+SECRET_KEY = 'SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

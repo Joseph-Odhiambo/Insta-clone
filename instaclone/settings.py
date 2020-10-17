387a818efe77aa11574474745dd18cbe1b323365
Joseph-Odhiambo <josephkdo@gmail.com>
2020-10-17 15:41:15 +0300

    class post and class profile update

--- instaclone/settings.py
+++ instaclone/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY ='0ty2o7sny@z_8s*_90abx5j!+6(^rt50*68lut-@(e*evm^pp!'
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -32,6 +32,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'instagram',
+    'crispy_forms',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- instagram/views.py
+++ instagram/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
+from .forms import SignUpForm
+from django.contrib.auth import login, authenticate
+from django.contrib import messages
+from django.shortcuts import render, redirect, get_object_or_404
+
 
 # Create your views here.
 def signup(request):
-    pass
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            messages.success(request, f'Account created for {username}!')
+            return redirect('index')
+    else:
+        form = SignUpForm()
+    return render(request, 'registration/signup.html', {'form': form})
+

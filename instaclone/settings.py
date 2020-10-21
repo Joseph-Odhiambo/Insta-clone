0b5b355a80f53889a4abdee174ec28d2b6f89828
Joseph-Odhiambo <josephkdo@gmail.com>
2020-10-21 15:00:00 +0300

    heroku

--- instaclone/settings.py
+++ instaclone/settings.py
@@ -157,7 +157,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 LOGIN_REDIRECT_URL = 'index'
